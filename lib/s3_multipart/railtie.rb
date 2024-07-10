a5df660eba7dcdc16f1da58c57f6f4d550d581c1
cbmike <mike@cubebrush.co>
2024-07-10 19:10:52 +0300

    Rails 7 compatibility. Uploaders must be required after action_mailer is loaded (at this point Upload model of the gem is accessible), otherwise it does not work

--- lib/s3_multipart/railtie.rb
+++ lib/s3_multipart/railtie.rb
@@ -2,7 +2,7 @@ if defined?(Rails)
   module S3Multipart
     class Railtie < Rails::Railtie
 
-      initializer "s3_multipart.action_view" do 
+      initializer "s3_multipart.action_view" do
         ActiveSupport.on_load :action_view do
           require 's3_multipart/action_view_helpers/form_helper'
         end
@@ -14,13 +14,11 @@ if defined?(Rails)
 
       # Load all of the upload controllers in app/uploaders/multipart
       initializer "s3_multipart.load_upload_controllers" do
-        begin
+        ActiveSupport.on_load :action_mailer do
           uploaders = Dir.entries(Rails.root.join('app', 'uploaders', 'multipart').to_s).keep_if {|n| n =~ /uploader\.rb$/}
           uploaders.each do |uploader|
             require "#{Rails.root.join('app', 'uploaders', 'multipart')}/#{uploader}"
           end
-        rescue
-          # Give some sort of error in the console
         end
       end
 
